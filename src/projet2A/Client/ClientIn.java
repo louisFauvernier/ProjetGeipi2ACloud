781ee247e5f8554861d90442a7d9e06bdbfc6af5
louis <loulou.fauvernier@hotmail.fr>
2014-03-06 11:20:40 +0100

    Définition des fonctions dans Client

--- src/projet2A/Client/ClientIn.java
+++ src/projet2A/Client/ClientIn.java
@@ -1,5 +1,47 @@
+/**
+ * Classe ClientIn
+ * 
+ * @author FAUVERNIER Louis, MENET Nicolas
+ */
+
+
 package projet2A.Client;
 
-public class ClientIn {
-	// TODO
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import projet2A.commonFiles.Fichier;
+
+public class ClientIn extends Thread {
+	private Socket socket;
+	private BufferedReader in;
+	
+	public ClientIn(String serverIp, int i) throws UnknownHostException, IOException{
+		socket = new Socket(serverIp, i);
+		in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+	}
+	
+	public void run(){
+		// TODO
+	}
+	
+	/**
+	 * Fonction de reception d'un fichier depuis le serveur
+	 * @return
+	 */
+	public Fichier rcvFichier(){
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Fonction d'enregistrement du fichier sur l'ordinateur (local)
+	 * @return
+	 */
+	public void saveFile(){
+		// TODO
+	}
 }
--- src/projet2A/Client/ClientOut.java
+++ src/projet2A/Client/ClientOut.java
@@ -1,8 +1,61 @@
+/**
+ * Classe ClientOut
+ * 
+ * @author FAUVERNIER Louis, MENET Nicolas
+ */
+
+
 package projet2A.Client;
 
 import java.net.Socket;
 
-public class ClientOut {
+import projet2A.commonFiles.Fichier;
+
+public class ClientOut extends Thread{
 	private Socket socket;
-	// TODO
+	private int i = 8080;
+	private String name;
+	
+	public ClientOut(String name) {
+		this.name = name;
+	}
+	public ClientOut(int port, String name) {
+		this.i = port;
+		this.name = name;
+	}
+	
+	public Socket getSocket() {
+		return socket;
+	}
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	
+	public void run(){
+		// TODO
+	}
+	
+	/**
+	 * Création d'un objet fichier (uniquement pour un nouveau fichier)
+	 * @param name Nom du fichier
+	 * @return objet correspondant au fichier
+	 */
+	public Fichier newFile(String name){
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Envoie du fichier f au serveur
+	 * @param f
+	 */
+	public void sendFile(Fichier f){
+		// TODO
+	}	
 }
